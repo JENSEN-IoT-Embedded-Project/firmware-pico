80776f62f843430129a589e4f0a9b62c30e44f09
Narunya <abzakhnartan@gmail.com>
2025-03-24 23:17:26 +0100

    updates to motion sensor code

--- naru/motionsensor.c
+++ naru/motionsensor.c
@@ -1,8 +1,42 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <mosquitto.h>
+
+void mosq() {
+    int rc;
+    struct mosquitto *mosq; // mosq library
+
+    mosquitto_lib_init(); //to use mosq.h
+
+    mosq = mosquitto_new("pico-w", true, NULL); // name, clean session true, false kom ihåg data, null (ingen custom data)
+    if (!mosq) {
+        printf("can't start mosq\n");
+    }
+
+    rc = mosquitto_connect(mosq, "192.168.1.231", 1883, 10);    // Connect to the broker (IP, port, keepalive)
+    if (rc != MOSQ_ERR_SUCCESS) { // om returncode inte är (predefined success i mosq lib) så return typ av error code
+        printf("can't connect, return code: %d\n", rc);
+        mosquitto_destroy(mosq);
+        mosquitto_lib_cleanup();
+    }
+
+    mosquitto_loop_start(mosq);
+
+    // struct, message id(null = autoassign), topic, size in bytes, message, QoS(1 gång), om brokern ska komma ihåg(false for now) 
+    rc = mosquitto_publish(mosq, NULL, "test", 17, "sensor triggered!", 1, false);
+    if (rc != MOSQ_ERR_SUCCESS) {
+        printf("failed to publish message, return code: %d\n", rc);
+    }
+    
+    sleep(2);
+
+}
 
 const uint trigger = 2;
 const uint echo = 3;
+
 
 float measureDistance() {
     gpio_put(trigger, 0);
@@ -31,16 +65,20 @@ int main() {
     gpio_set_dir(trigger, GPIO_OUT);
     gpio_set_dir(echo, GPIO_IN);
 
-    printf("Distance measurement system active...\n");
-    
     while (true) {
         float distance = measureDistance();
-        printf("Distance: %.2f cm\n", distance);
 
         if (distance < 10) {
-            printf("Intruder detected!\n");
+            mosq();
+            sleep_us(1000);
         }
         sleep_ms(100);
     }
+
+    mosquitto_disconnect(mosq);
+    mosquitto_destroy(mosq);
+    mosquitto_lib_cleanup();
     return 0;
 }
+
+
