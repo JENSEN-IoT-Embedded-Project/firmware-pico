ebb2c683ec572878ec9f9d21e1dc31ff38a1e88e
Ludde <luddenorin@gmail.com>
2025-03-25 17:14:24 +0100

    Feat: Motionsensor.py/modules & motionsensor.py/main.py created modules dir and wrote one function to get the HC-SR04 ULTRASONIC sensor running just measuring distance. need to add error messages if something is wrong

---
+++ ludde/motionsensor/motionsensor_py/main.py
@@ -0,0 +1,5 @@
+from modules import hcsr04
+
+
+while True:
+    print(hcsr04.distance_to_object(15,14))
---
+++ ludde/motionsensor/motionsensor_py/modules/hcsr04.py
@@ -0,0 +1,26 @@
+from machine import Pin
+import utime
+
+
+def distance_to_object(pin_out,pin_in) :
+    trigger = Pin(pin_out, Pin.OUT)
+    echo = Pin(pin_in,Pin.IN)
+
+    trigger.low()
+    utime.sleep_us(2)
+    trigger.high()
+    utime.sleep_us(10)
+    trigger.low()
+
+    while echo.value() == 0 :
+        signalOff = utime.ticks_us()
+    while echo.value() == 1 :
+        signalOn = utime.ticks_us()
+
+    timePassed = signalOn - signalOff
+    distance_cm = (timePassed * 0.0343)/2
+    utime.sleep(1)
+    return distance_cm
+
+
+
