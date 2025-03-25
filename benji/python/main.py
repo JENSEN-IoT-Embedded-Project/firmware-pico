b56d8debabd732d00a42dd389c358640c4d27686
Benji <ben.flodin@outlook.com>
2025-03-25 17:38:59 +0100

    Update main.py

--- benji/python/main.py
+++ benji/python/main.py
@@ -1,6 +1,27 @@
-from modules import hcsr04
+from modules import alarm, hcsr04, joystick_pos, LCD_led
+import utime
 
+isArmed = False
+counter = 0
 
-# MAIN LOOP
-while 1:
+LCD_led.display_led_screen(counter)
 
+while True:
+    utime.sleep_ms(10)
+    
+    distance = hcsr04.measure_distance()
+    if distance < 20:
+        if isArmed:
+            alarm.play()
+    
+    thumbstick_command = None
+    thumbstick_command = joystick_pos.get_thumbstick_position()
+    
+    if thumbstick_command is not None:
+        
+        if thumbstick_command == "button_pressed":
+            isArmed = not isArmed
+        counter = LCD_led.led_screen(thumbstick_command, counter)
+        LCD_led.display_led_screen(counter)
+    
+    utime.sleep_ms(100)
