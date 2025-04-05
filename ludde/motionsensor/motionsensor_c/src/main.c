5e2d7018b6d03a87bd08d93294bf108d69aefa4b
Ludde <luddenorin@gmail.com>
2025-04-05 22:48:50 +0200

    main.c not working atm

--- ludde/motionsensor/motionsensor_c/src/main.c
+++ ludde/motionsensor/motionsensor_c/src/main.c
@@ -1,11 +1,13 @@
 #include "pico/stdlib.h"
-#include "stdio.h"
-#define TRIGG 15
-#define ECHO 14
-double sonic_sensor(uint *trigger_pin, uint *echo_pin);
+#include <stdio.h>
+#include "pico/time.h"
+#include <inttypes.h>
+double sonic_sensor(const uint *trigger_pin,const uint *echo_pin);
 int main() {
 	stdio_init_all();
     //initialize gpio for trigger and echo pins and set directon
+    const uint TRIGG = 15;
+    const uint ECHO = 14;
 	gpio_init(TRIGG);
 	gpio_init(ECHO);
 	gpio_set_dir(TRIGG,1);
@@ -13,43 +15,37 @@ int main() {
 	double distance = 0;
 
 	while(1) {
-		distance = sonic_sensor(TRIGG,ECHO);
-		printf("%d",distance);
+		distance = sonic_sensor(&TRIGG,&ECHO);
 	}
-	
+    return 0;
 }
-
-double sonic_sensor(uint *trigger_pin, uint *echo_pin) {
-	double distance = 0;
-	//send out and turn off signal to trigger pin.
-	gpio_put(trigger_pin,0);
+double sonic_sensor(const uint *trigger_pin,const uint *echo_pin) {
+	double distance = 1;
+    //send out singal to trigger pin
+    gpio_put(*trigger_pin, 0);
+    sleep_ms(2);
+	gpio_put(*trigger_pin,1);
 	sleep_ms(10);
-	gpio_put(trigger_pin,1);
-	sleep_ms(10);
-	gpio_put(trigger_pin,0);
-	sleep_ms(2);
-	int time_low = 0; 
-	int time_high = 0;
-	absolute_time_t timer_start;
-	absolute_time_t timer_end;
-	while(gpio_get(echo_pin) == 0){
-		timer_start = get_absolute_time();
-	}
+    gpio_put(*trigger_pin,0);
+	absolute_time_t time_start;
+	absolute_time_t time_end;
+    int64_t us_low = 0;
+    int64_t us_high = 0;
+    //count and return time while echo pin is not reciving data
+	while(gpio_get(*echo_pin) == 0){
+        printf("Echo pin off\n");
+		time_start = get_absolute_time();
+	} 
 	time_end = get_absolute_time();
-
-	while(gpio_get(echo_pin) == 1) {
+    us_low = absolute_time_diff_us(time_start,time_end);   
+    printf("microseconds low: %lld\n",us_low);
+	while(gpio_get(*echo_pin) == 1) {
+       printf("Echo pin on\n");
+       time_start = get_absolute_time(); 
 	}
-	tick_stop(time_high);
-
-	time_passed = time_low - time_high;
-
-	distance = time_passed algoritm för ljudets hastighet / 2 
-
-
-
-
-	
-
+    time_end = get_absolute_time();
+    us_high = absolute_time_diff_us(time_start,time_end);
+    printf("microseconds high: %lld\n", us_high);
 
 	return distance;
 
--- ludde/motionsensor/motionsensor_py/main.py
+++ ludde/motionsensor/motionsensor_py/main.py
@@ -7,9 +7,11 @@ lcd = init_lcd(2,16,0,1)
 buzzer = Pin(13,Pin.OUT)
 itr = 0
 max_diff = 5;
+btn = Pin(17,Pin.IN,Pin.PULL_UP)
+alarm_activated = False
 
 while True:
-
+        
         distance = round(distance_to_object(15,14),2) 
         if itr == 0 :
             itr += 1
@@ -18,9 +20,13 @@ while True:
         display_lcd(lcd,"Distance", distance, 16)
 
         if(initial_distance  - distance) > max_diff :
-            while True :
+            alarm_activated = True
+            while alarm_activated :
                 display_lcd(lcd,"WARNING","ALARM ACTIVATED",16)
                 buzzer_signal(buzzer)
+                if btn.value() == 0 :
+                    itr = 0
+                    alarm_activated = False
 
 
 
