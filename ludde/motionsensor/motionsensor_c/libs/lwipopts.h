222c0ee0e6a5e1064e9dda4a446e81d75e78cc8f
Ludde <luddenorin@gmail.com>
2025-04-09 16:27:36 +0200

    tests/wifi.c test code for connecting pico to wifi completed and tested

---
+++ ludde/motionsensor/motionsensor_c/libs/lwipopts.h
@@ -0,0 +1,15 @@
+#ifndef _LWIPOPTS_H
+#define _LWIPOPTS_H
+
+#define NO_SYS 1
+#define LWIP_SOCKET 0
+#define LWIP_NETCONN 0
+#define MEM_ALIGNMENT 4
+#define LWIP_NETIF_HOSTNAME 1
+#define LWIP_DHCP 1
+#define LWIP_IPV4 1
+#define LWIP_TCP 1
+#define LWIP_UDP 1
+
+#endif /* _LWIPOPTS_H */
+
