e50171b9aec0148e6fca400aa8f8bac68e966b69
Ludde <luddenorin@gmail.com>
2025-05-03 16:07:45 +0200

    started building motionsensor library functions missing for calibration set_reference_value

--- ludde/motionsensor/motionsensor_c/src/main.c
+++ ludde/motionsensor/motionsensor_c/src/main.c
@@ -1,1 +1,33 @@
+#include "motionsensor.h"
+#include "pico/stdlib.h"
+#include "pico/time.h"
+#include <stdio.h>
+#include <stdlib.h>
 
+
+int main() {
+    stdio_init_all();
+    const uint TRIGG = 15;
+    const uint ECHO = 14;
+    gpio_init(TRIGG);
+    gpio_init(ECHO);
+    gpio_set_dir(TRIGG,1);
+    gpio_set_dir(ECHO,0);
+    int size = 10;
+    double reference_level = 0;
+    double* calibration_data = run_callibration(TRIGG,ECHO,size);
+
+    if(!calibration_quality_ok(calibration_data,size,2)){
+            printf("WARNING:Callibration failed");
+            free(calibration_data);
+            return 1;
+    }
+    printf("Callibration completed succesfully\n");
+
+    for(int i = 0; i < size; i++ ) {
+        printf("%.2f\n", calibration_data[i]);
+    }
+
+    free(calibration_data);
+    return 0;
+}
---
+++ ludde/motionsensor/motionsensor_c/src/motionsensor.c
@@ -0,0 +1,67 @@
+#include "motionsensor.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "pico/stdlib.h"
+#include "pico/time.h"
+
+//make sure to free memory when used
+double* run_callibration(const uint pin1, const uint pin2,int size) {
+    stdio_init_all();
+    double* ref_values = (double*)malloc(size * sizeof(double));
+    if(ref_values == NULL) {
+        printf("Memory allocation failed");
+        exit(1);
+    }
+    double distance = 0;
+    uint64_t time_passed = 0;
+    absolute_time_t time_start;
+    absolute_time_t time_end;
+
+    for(int i = 0; i < size; i++){
+        printf("loop %d\n",i);
+        sleep_ms(1000);
+        gpio_put(pin1,1);
+        sleep_us(10);
+        gpio_put(pin1,0);
+
+        while(!gpio_get(pin2)) {
+            tight_loop_contents();
+        }
+        time_start = get_absolute_time();
+        
+        while(gpio_get(pin2)) {
+            sleep_us(1);
+        }
+        time_end = get_absolute_time();
+
+        time_passed = absolute_time_diff_us(time_start,time_end);
+        distance =(time_passed * 0.0343)/2.0;
+        printf("%.2f\n",distance);
+        ref_values[i] = distance;
+        sleep_ms(1000);
+    }
+    return ref_values;
+};
+
+int calibration_quality_ok(double* arr, int size, int tolerance_value) {
+    //sort the array
+    for(int i = 0 ; i < size -1 ; i++) {
+        for(int j = 0; j < size-1; j++) {
+            if(arr[j] > arr[j+1]) {
+                double temp = arr[j]; 
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp; 
+            }
+        }
+    }
+    if((arr[size-1] - arr[0]) > tolerance_value) {
+        return 0;
+    }
+    return 1;
+}
+
+/*void set_reference_level(double* value,double arr[],size) {
+    for(int i = 0; i < size; i++*/
+
+
+
