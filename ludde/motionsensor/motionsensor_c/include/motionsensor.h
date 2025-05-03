585b8c7f66207b9162905e5e0c83be65ed47bb78
Ludde <luddenorin@gmail.com>
2025-05-03 16:07:45 +0200

    started building motionsensor library functions missing for calibration set_reference_value

---
+++ ludde/motionsensor/motionsensor_c/include/motionsensor.h
@@ -0,0 +1,12 @@
+#ifndef MOTIONSENSOR_H
+#define MOTIONSENSOR_H
+
+#include "pico/stdlib.h"
+double* run_callibration(const uint pin1, const uint pin2,int size);
+int calibration_quality_ok(double* arr, int size, int tolerance_value);
+#endif
+
+
+    
+
+
