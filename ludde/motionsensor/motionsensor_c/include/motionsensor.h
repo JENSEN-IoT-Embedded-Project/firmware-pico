c60106550ff7709313220cbb58e7cb6c4e0fa640
Ludde <luddenorin@gmail.com>
2025-05-04 22:04:53 +0200

    motionsensor.c:function motion_scan first draft completed

--- ludde/motionsensor/motionsensor_c/include/motionsensor.h
+++ ludde/motionsensor/motionsensor_c/include/motionsensor.h
@@ -2,9 +2,10 @@
 #define MOTIONSENSOR_H
 
 #include "pico/stdlib.h"
-double* run_callibration(const uint pin1, const uint pin2,int size);
+double* run_callibration(const uint gpio_out, const uint gpio_in,int size);
 int calibration_quality_ok(double* arr, int size, int tolerance_value);
 void set_reference_level(double *value,double arr[],int size);
+int motion_scan(const uint gpio_out, const uint gpio_in, double reference_level , double tolerance_level);
 #endif
 
 
