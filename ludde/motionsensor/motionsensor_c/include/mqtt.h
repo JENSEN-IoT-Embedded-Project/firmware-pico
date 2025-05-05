238180f300292040fb11fcceff604376a966ee28
Ludde <luddenorin@gmail.com>
2025-05-05 16:53:36 +0200

    bug fix mqtt.c

--- ludde/motionsensor/motionsensor_c/include/mqtt.h
+++ ludde/motionsensor/motionsensor_c/include/mqtt.h
@@ -6,7 +6,7 @@
 #include "pico/cyw43_arch.h"
 
 void mqtt_connection_cb(mqtt_client_t *client, void *arg,mqtt_connection_status_t status);
-int mqtt_connect(mqtt_client_t *client, const char *server_id, const char*client_id,struct mqtt_connect_client_info_t *info);
+int mqtt_connect(mqtt_client_t **client, const char *server_id, const char*client_id,struct mqtt_connect_client_info_t *info);
 void mqtt_pub_motion_read(mqtt_client_t *client, const char *topic,const char* message);
 
 #endif
