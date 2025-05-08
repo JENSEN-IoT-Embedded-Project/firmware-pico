6fdc8a63f5d5308e790cfc647e8ef8f23ec36ecb
Narunya <abzakhnartan@gmail.com>
2025-05-08 11:53:27 +0200

    mqtt

---
+++ naru/c_motion/include/mqtt_config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+
+#define _MQTT_BROKER_IP "192.168.1.231"
+#define _MQTT_PORT 1883
+
+#ifndef MQTT_DEVICE_NAME
+#define MQTT_DEVICE_NAME "pico-w" // change name on your device if you want..
+
+#endif
